785fc083e0b03af049b8618f7a572fde9a5c28ef
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-04-26 21:26:07 +0300

    Update 100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19")
+import sys
+str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write(str)
 sys.stderr.flush()
