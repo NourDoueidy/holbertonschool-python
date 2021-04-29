167552fee723789dfcf8e0ed6524056754e863c7
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-04-29 12:41:07 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv) -1
-if n == 0:
-    print("{}".format(n))
-for i in range(1, len(sys.argv)):
-    sum += int(sys.argv[i])
-print(sum)
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv) -1
+    if n == 0:
+        print("{}".format(n))
+    for i in range(1, len(sys.argv)):
+        sum += int(sys.argv[i])
+    print(sum)
