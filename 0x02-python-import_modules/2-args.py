6ea9f8a3f5e5edd0f7ce7e39942618711b0224ab
NourDoueidy <nourdoueidy@hotmail.com>
2021-04-29 11:18:28 +0300

    fixing code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import sys
 
-n = len(argv) - 1
+n = len(sys.argv) - 1
 if n == 0:
     print("0 arguments.")
 elif n == 1:
     print("1 argument:")
 else :
     print("{} arguments:".format(n))
-while i <= n:
-    print("{}: {}".format(i,argv[i]))
-    i += 1
+for i in range(n):
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    
