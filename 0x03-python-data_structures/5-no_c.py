5e1564473f1d27619f83ecf92a454f3218dd74d7
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-05-03 20:48:42 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    for i in len(my_strin):
+    for i in len(my_string):
         if i =! "c" and i =! "C":
             new_string =+ i
             print("{}".format(new_string))
