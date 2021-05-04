0c55b2034438a197904c162c3098a4e8321a3d7a
NourDoueidy <nour.doueidy@gmail.com>
2021-05-04 16:41:35 +0300

    completed task

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == {}:
+        return None
+    max_key = None
+    max_value = 0
+    for a, b in a_dictionary.items():
+        if a > max_value:
+            max_value = a
+            max_key = b
+    return max_key
