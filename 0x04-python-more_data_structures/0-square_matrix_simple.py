13b8120e537f5240116eca92e163398a58dbd854
NourDoueidy <nourdoueidy@hotmail.com>
2021-05-04 10:28:09 +0300

    fixing code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    for i in range (len(matrix)):
-        for j in range (len(matrix[i])):
-            return j ** 2
+    new_matrix = []
+    for x in matrix:
+        new_matrix.append(lambda x: x ** 2)
+    return new_matrix
