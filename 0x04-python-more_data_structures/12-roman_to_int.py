a66fdf5bdfbcaab1ed3536f56b4df4e42f8ccc2e
NourDoueidy <nour.doueidy@gmail.com>
2021-05-04 17:57:43 +0300

    completed task

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    result = 0 
+    x = 0
+    roman_dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    for i in reversed(roman_string):
+        x = roman_dic[i]
+        if result < x * 5:
+            result += i
+        else:
+            result -= i
+    return result
