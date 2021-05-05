fc3d4deee9ea27bc5735e7d562dcb1734dd4dccf
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-05-05 07:27:38 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-    result = 0 
+    result = 0
     x = 0
     roman_dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for i in reversed(roman_string):
