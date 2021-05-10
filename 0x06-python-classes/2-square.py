787d15a16e67d42a25e88b85311ead0b3a49756a
NourDoueidy <nour.doueidy@gmail.com>
2021-05-10 21:25:37 +0300

    completed task

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square module."""
+class Square:
+    """Defines a square."""
+    def __init__(self, size=0)
+        """Initialize a new square.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
