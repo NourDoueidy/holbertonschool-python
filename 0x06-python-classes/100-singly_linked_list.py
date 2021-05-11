ce212753b2816fc8fc166edc33a21f5120324f06
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-05-11 13:22:46 +0300

    Update 100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,10 +6,11 @@ class Node:
     def __init__(self, data, next_node=None):
         """initialize square.
         Args:
-            data
-            next_node
+            data (int)
+            next_node (Node)
         """
         self.data = data
+        self.next_node = next_node
     
     @property
     def data(self):
@@ -35,9 +36,32 @@ class Node:
 
 
 class SinglyLinkedList:
+    """Define a singly linked list"""
     def __init__(self):
-        self.head = None
+        """initiate a new Node.
+        Args:
+            value (Node): The new Node to insert.
+        """
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
 
-        
-
-
+    def __str__(self):
+        """Define the print() representation of a SinglyLinkedList."""
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(values))
