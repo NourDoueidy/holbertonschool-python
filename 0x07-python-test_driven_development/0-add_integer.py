14ab82b149d007dcbc7579a0e57a48d82738a426
NourDoueidy <nour.doueidy@gmail.com>
2021-05-13 08:40:51 +0300

    fixing code

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""Defines an addition function"""
+"""Defines an integer addition function."""
+
 
 def add_integer(a, b=98):
-    """Return addition of integers a and b.
-    Floats are transformed to ints before adding args.
-    Raises: 
-      TypeError if either a or b is non-integer and non-flat"""
-
-    if not isinstance(a, int) and not isinstance(a, float):
+    """Return the integer addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
+    Raises:
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    
+    return (int(a) + int(b))
