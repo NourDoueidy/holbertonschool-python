b31d7e636793d9d345f84e488dc924be79db1b40
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-05-14 19:26:08 +0300

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,7 +2,7 @@
 """Defines a matrix division function"""
 
 def matrix_divided(matrix, div):
-    """Divide all element of a matrix.
+    """Divide all elements of a matrix.
     Args:
         matrix (list): A list of lists of ints or floats.
         div (int/float): The divisor.
@@ -14,22 +14,20 @@ def matrix_divided(matrix, div):
     Returns:
         A new matrix representing the result of the division.
     """
-    
-    if not isinstance(div, (int, float)):
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
 
-    if not isinstance(matrix, list) or len(matrix) == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    for row in matrix:
-        if not isinstance(row, list) or len(row) == 0:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-        for x in row:
-        if not isinstance(x, (int, float)):    
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
-           
-    return [[round(x / div, 2) for x in row] for row in matrix]
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
