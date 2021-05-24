ccc0b7d7b6e3d1dd214f0e2623f509365a74663a
NourDoueidy <nour.doueidy@gmail.com>
2021-05-24 19:00:40 +0300

    completed task

---
+++ 0x0B-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for class Square subclass of Rectangle"""
+Rectangle = __import__("9-rectangle.py").Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize a square"""
+        self.__size = size
+        self.integer-validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """Calculates area"""
+        return self.__size ** 2
