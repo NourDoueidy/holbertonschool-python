a80f40fe2881d28b2049da85cab3464a4c46bef2
NourDoueidy <nourdoueidy@hotmail.com>
2021-05-24 11:18:34 +0300

    completed task

---
+++ 0x0B-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for same class"""
+
+
+def is_same_class(obj, a_class):
+    """Returns true or false if same class"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
