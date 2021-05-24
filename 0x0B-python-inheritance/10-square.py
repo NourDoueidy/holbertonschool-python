364f1962ab79b34431ac68c50409a056af12ecb4
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-05-24 19:05:27 +0300

    Update 10-square.py

--- 0x0B-python-inheritance/10-square.py
+++ 0x0B-python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Module for class Square subclass of Rectangle"""
-Rectangle = __import__("9-rectangle.py").Rectangle
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
