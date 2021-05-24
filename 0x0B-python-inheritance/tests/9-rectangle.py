fc816b3ffe2db255a9a200c2cc3bd1d71c41750e
NourDoueidy <nourdoueidy@hotmail.com>
2021-05-24 13:20:07 +0300

    test

---
+++ 0x0B-python-inheritance/tests/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for class Rectangle"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines a class inheriting from BaseGeometry"""
+    def __init__(self, width, height):
+        """constructor"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self, width, height):
+        """calculates area"""
+        return self.__width * self.__height
+
+    def __repr__(self):
+        """prints width and height of rectangle"""
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
