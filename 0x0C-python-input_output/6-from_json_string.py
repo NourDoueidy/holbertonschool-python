d743b1e59aaa2995f3a7ae35ba0a2f8e9d9d4643
NourDoueidy <nour.doueidy@gmail.com>
2021-05-25 16:19:55 +0300

    completed task

---
+++ 0x0C-python-input_output/6-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for json string"""
+
+import jason
+
+
+def from_json_string(my_str):
+    """Returns an object (python data structure) represented by json string"""
+    return jason.loads(my_str)
