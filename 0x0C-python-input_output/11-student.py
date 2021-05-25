d8bc421d6d13f8cc71203bee839e9aee23474d0d
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-05-25 20:45:44 +0300

    Update 11-student.py

--- 0x0C-python-input_output/11-student.py
+++ 0x0C-python-input_output/11-student.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Module for class Student"""
+"""Defines a class Student."""
 
 
 class Student:
-    """Defines a class"""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a student.
-        
+        """Initialize a new Student.
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
@@ -18,5 +17,5 @@ class Student:
         self.age = age
 
     def to_json(self):
-        """Retrieves a dictionary representation of class Student"""
+        """Get a dictionary representation of the Student."""
         return self.__dict__
