73784f46d295c69e8f06efc16fb50fdb70585763
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-05-25 11:28:02 +0300

    Update 0-read_file.py

--- 0x0C-python-input_output/0-read_file.py
+++ 0x0C-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Module for file reading function"""
 
+
 def read_filename(filename=""):
     """function that reads a textfile and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read(),end="")
+        print(f.read(), end="")
