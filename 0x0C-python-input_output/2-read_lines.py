0bf10f73b8cb9a9479f8ce7d863a23318f9e8d7d
NourDoueidy <nourdoueidy@hotmail.com>
2021-05-25 12:37:34 +0300

    completed task

---
+++ 0x0C-python-input_output/2-read_lines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for reading lines function"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """Function that reads n lines of a text file and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        if nb_lines <=0:
+            print(f.read(), end="")
+                return
+
+        count = 0
+        for line in f:
+            if count < nb_lines:
+                print(line, end="")
+            count += 1
