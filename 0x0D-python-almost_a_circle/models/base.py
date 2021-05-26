af7109691fef8a00a57ee9b96d3d48fd4e0acde9
NourDoueidy <nourdoueidy@hotmail.com>
2021-05-26 13:03:54 +0300

    completed task

---
+++ 0x0D-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for class base"""
+
+
+class Base:
+    """Defines base"""
+
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """Constructor"""
+        if id ist not None:
+            self.id = if
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
