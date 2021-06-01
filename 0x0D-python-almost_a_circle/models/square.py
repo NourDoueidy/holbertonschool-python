58a05911063a33eaab0f67b08978b70f2384e8b7
NourDoueidy <nour.doueidy@gmail.com>
2021-06-01 18:08:53 +0300

    update

--- 0x0D-python-almost_a_circle/models/square.py
+++ 0x0D-python-almost_a_circle/models/square.py
@@ -13,3 +13,19 @@ class Square(Rectangle):
     def __str__(self):
         """string representation of square"""
         return "[{}] ({}) {}/{} - {}".format(type(self).__name__, self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """Updates class"""
+        if args:
+            if len(args) >= 2:
+                self.__size = args[0]
+            if len(args) >= 2:
+                self.__size = args[1]
+            if len(args) >= 3:
+                self.__x = args[2]
+            if len(args) >= 4:
+                self.__y = args[3]
+                
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
