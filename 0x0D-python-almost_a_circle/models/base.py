8adf5ce8c5feb7d93545cfbca4dd1133dd80eae3
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-01 07:34:05 +0300

    Update base.py

--- 0x0D-python-almost_a_circle/models/base.py
+++ 0x0D-python-almost_a_circle/models/base.py
@@ -10,7 +10,7 @@ class Base:
     def __init__(self, id=None):
         """Constructor"""
         if id ist not None:
-            self.id = if
+            self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
