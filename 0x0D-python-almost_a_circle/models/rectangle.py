abdbbabe55479b67007c49a62eb72403ffb8a46b
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-01 12:37:17 +0300

    Update rectangle.py

--- 0x0D-python-almost_a_circle/models/rectangle.py
+++ 0x0D-python-almost_a_circle/models/rectangle.py
@@ -7,18 +7,7 @@ class Rectangle(Base):
     """Represent a rectangle."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new Rectangle.
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-            x (int): The x coordinate of the new Rectangle.
-            y (int): The y coordinate of the new Rectangle.
-            id (int): The identity of the new Rectangle.
-        Raises:
-            TypeError: If either of width or height is not an int.
-            ValueError: If either of width or height <= 0.
-            TypeError: If either of x or y is not an int.
-            ValueError: If either of x or y < 0.
+        """Initialize a new Rectangle
         """
         self.width = width
         self.height = height
@@ -92,18 +81,16 @@ class Rectangle(Base):
 
     def update(self, *args):
         """Updates class"""
+        a = 0
         for arg in args:
-                if a == 0:
-                    if arg is None:
-                        self.__init__(self.width, self.height, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif a == 1:
-                    self.width = arg
-                elif a == 2:
-                    self.height = arg
-                elif a == 3:
-                    self.x = arg
-                elif a == 4:
-                    self.y = arg
-                a += 1
+            if a ==0:
+                self.id = arg
+            elif a == 1:
+                self.width = arg
+            elif a == 2:
+                self.height = arg
+            elif a == 3:
+                self.x = arg
+            elif a == 4:
+                self.y = arg
+            a += 1    
