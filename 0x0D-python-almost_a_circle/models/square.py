e4cf853ef23748fa4407aa8475a1c056668f63f9
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-01 07:22:41 +0300

    Update square.py

--- 0x0D-python-almost_a_circle/models/square.py
+++ 0x0D-python-almost_a_circle/models/square.py
@@ -9,6 +9,10 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Constructor"""
         super().__init__(size, size, x, y, id)
+    
+    def __str__(self):
+        """Return string representation of a square"""
+        return "[{}] ({}) {}/{} - {}".format(type(self).__name__, self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
@@ -20,8 +24,3 @@ class Square(Rectangle):
         """Setter for size"""
         self.width = value
         self.height = value
-
-    def __str__(self):
-        """Return string representation of a square"""
-        return "[{}] (<{}>) <{}>/<{}> - <{}>".format(type(self).__name__, self.id, self.x, self.y, self.width)
-
