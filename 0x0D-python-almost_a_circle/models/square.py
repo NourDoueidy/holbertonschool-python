f599bafb30e6eebaf0adab8af60091da7aee4646
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-01 07:24:20 +0300

    Update square.py

--- 0x0D-python-almost_a_circle/models/square.py
+++ 0x0D-python-almost_a_circle/models/square.py
@@ -24,3 +24,27 @@ class Square(Rectangle):
         """Setter for size"""
         self.width = value
         self.height = value
+        
+    def update(self, *args, **kwargs):
+        """Update method for Square"""
+
+        if args:
+            for i, element in enumerate(args):
+                if i == 0:
+                    self.id = element
+                elif i == 1:
+                    self.size = element
+                elif i == 2:
+                    self.x = element
+                elif i == 3:
+                    self.y = element
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key) is True:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """returns the dictionary representation of a Square"""
+
+        my_dict2 = {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        return my_dict2    
