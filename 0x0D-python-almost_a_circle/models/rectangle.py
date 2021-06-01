dba6768ef75e75350e1ae539f4c9a75a26613463
NourDoueidy <nour.doueidy@gmail.com>
2021-06-01 19:04:14 +0300

    update

--- 0x0D-python-almost_a_circle/models/rectangle.py
+++ 0x0D-python-almost_a_circle/models/rectangle.py
@@ -96,3 +96,8 @@ class Rectangle(Base):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
+        dictionary = {"id":self.id, "width":self.width, "height":self.height, "x":self.x, "y":self.y}
+        return dictionary
