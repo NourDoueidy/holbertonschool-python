6f2292b226a2fdcdfeb2dcf9d9c551f76a95b6af
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-07 13:11:43 +0300

    Update 1-hbtn_header.py

--- 0x10-python-network_1/1-hbtn_header.py
+++ 0x10-python-network_1/1-hbtn_header.py
@@ -1,8 +1,7 @@
 #!usr/bin/python3
 """Sends a request to the URL and displays the value of X_Request-Id"""
-
+from urllib import request
 import sys
-import urllib.request
 
 if __name__ == "__main__":
     with request.urlopen(sys.argv[1]) as response:
