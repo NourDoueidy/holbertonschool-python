8a049d0fb2d2528e8ccfca49f2a01aeaa1481056
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-08 13:10:10 +0300

    Update 2-post_email.py

--- 0x10-python-network_1/2-post_email.py
+++ 0x10-python-network_1/2-post_email.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """sends post request to URL with email and displays the body of the response"""
 
-import urllib.request
 import sys
+import urllib.request
 import urllib.parse
 
-
 if __name__ == "__main__":
-    values = {"email" : sys.argv[2]}
+    values = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(values)
-    data = data.encode("utf-8")
+    data = data.encode('utf-8')
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as resp:
-        print(resp.read().decode("utf-8"))
+        print(resp.read().decode('utf-8'))
