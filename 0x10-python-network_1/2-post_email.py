77e483dc0e53a7ca889aba2597eaac09e169b391
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-08 13:16:18 +0300

    Update 2-post_email.py

--- 0x10-python-network_1/2-post_email.py
+++ 0x10-python-network_1/2-post_email.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-# Take in a URL and email, send POST request
-
+""" Fetches header from url passed as arg with email data"""
+from urllib import request, parse
 import sys
-import urllib.request
-import urllib.parse
 
 if __name__ == "__main__":
-    values = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('utf-8')
-    req = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(req) as res:
-        print(res.read().decode('utf-8'))
+    data = parse.urlencode({"email": sys.argv[2]}).encode()
+    with request.urlopen(sys.argv[1], data) as response:
+        print(response.read().decode('utf-8'))
