c374ba27b2acd2e685f013d245fe106c6064a57d
NourDoueidy <nourdoueidy@hotmail.com>
2021-06-10 12:58:09 +0300

    task

---
+++ 0x10-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Display the value of the variable X-Request-Id"""
+
+import requests
+import sys.argv
+
+
+if __name__ == "__main__":
+    resp = requests.get(argv[1])
+    print(response.headers.get("X-Request-Id"))
