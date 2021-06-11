4daffa351eb0ef2ea026824d2d9419a6d483b0cd
NourDoueidy <nour.doueidy@gmail.com>
2021-06-11 18:36:25 +0300

    task

---
+++ 0x13-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Create database hbtn_0d_usa
+-- Create table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities@hbtn_0d_usa (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL FOREIGN KEY,
+    name VARCHAR(256) NOT NULL
+    REFERENCES hbtn_0d_usa.states(id)
+);
