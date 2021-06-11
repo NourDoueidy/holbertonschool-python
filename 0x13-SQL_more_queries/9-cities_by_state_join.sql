b143d527e2bee578e64b12e900241bb152a3c6fc
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-11 20:10:56 +0300

    Update 9-cities_by_state_join.sql

--- 0x13-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x13-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- List all cities contained in hbtn_0d_usa
 -- join and order by cities.id
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.states_id ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
