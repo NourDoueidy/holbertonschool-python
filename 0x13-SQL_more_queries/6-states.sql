92193d5670e29c0a04be92421ff69b3573a422ed
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-11 12:59:35 +0300

    Update 6-states.sql

--- 0x13-SQL_more_queries/6-states.sql
+++ 0x13-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
--- Create database hbtn_0d_usa and table states.
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS 'states' (
-    id INT AUTO-INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+-- Creates the table hbtn_0d_usa with table states.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
 );
