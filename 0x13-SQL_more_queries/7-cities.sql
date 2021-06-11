47643e97acb42137b7742c61f27cbe4d66cde906
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-11 18:42:25 +0300

    Update 7-cities.sql

--- 0x13-SQL_more_queries/7-cities.sql
+++ 0x13-SQL_more_queries/7-cities.sql
@@ -2,9 +2,9 @@
 -- Create table cities
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
 CREATE TABLE IF NOT EXISTS 'cities'@'hbtn_0d_usa' (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
+    'id' INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    'state_id' INT NOT NULL,
+    'name' VARCHAR(256) NOT NULL,
     FOREIGN KEY ('state_id')
     REFERENCES 'hbtn_0d_usa'.'states'('id')
 );
