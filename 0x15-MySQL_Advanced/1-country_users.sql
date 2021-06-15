3e6646622243f2a86e5fc23ac9f012a1924dc937
NourDoueidy <nourdoueidy@hotmail.com>
2021-06-15 12:32:41 +0300

    completed task

---
+++ 0x15-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Country users
+-- Create a table users with attributes: id, email, name and country
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM(US, CO, TN) NOT NULL
+);
