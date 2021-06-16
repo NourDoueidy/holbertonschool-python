8dd100ed3feae5921f6d59d2f77001a04db65130
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-16 11:19:11 +0300

    Update 5-valid_email.sql

--- 0x15-MySQL_Advanced/5-valid_email.sql
+++ 0x15-MySQL_Advanced/5-valid_email.sql
@@ -1,8 +1,13 @@
 -- Valid email
 -- Creat a trigger that Resets valid_email if the email is changed
+DELIMITER $$
+
 CREATE TRIGGER res_email BEFORE UPDATE ON users
 FOR EACH ROW 
 BEGIN
     IF OLD.email <> NEW.email THEN
         SET NEW.valid_email = 0;
     END IF;
+END $$
+
+DELIMITER ;
