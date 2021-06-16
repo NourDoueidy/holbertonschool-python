e512b585daa032e8302963f2fd9ac7f9fd83d6f5
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-16 06:45:58 +0300

    Update 3-glam_rock.sql

--- 0x15-MySQL_Advanced/3-glam_rock.sql
+++ 0x15-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,4 @@
 -- Old school bands 
 -- List all bands with Glam as their main style, ranked by their longevity
-SELECT band_name, COALESCE(split, 2021) - formed as lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%' ;
