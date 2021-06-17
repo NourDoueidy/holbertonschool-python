478aa445f374bc1616462492ca00c4686bb31a0f
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-17 20:13:31 +0300

    Update 100-overall_weighted_score.sql

--- 0x15-MySQL_Advanced/100-overall_weighted_score.sql
+++ 0x15-MySQL_Advanced/100-overall_weighted_score.sql
@@ -1,10 +1,16 @@
 -- Overall weighted score
 -- Create a procedure ComputeOverallWeightedScoreForUser that computes and store the overall weighted score for a student.
-CREATE PROCEDURE computeOverallWeightedScoreForUser (IN user_id INT)
-UPDATE users set overall_score = (SELECT 
-SUN(corrections.score * projects.weight)
-FROM corrections
-INNER JOIN projects
-ON projects.id = corctions.project_id
-WHERE corrections.user_id = user_id)
-WHERE users.id = user_id;
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
+BEGIN
+    DECLARE sum_score FLOAT;
+    SET sum_score = (SELECT SUM(score * weight)
+                        FROM users AS U 
+                        JOIN corrections as C ON U.id=C.user_id 
+                        JOIN projects AS P ON C.project_id=P.id 
+                        WHERE U.id=user_id);
+    UPDATE users SET overall_score = sum_score WHERE id=user_id;
+END
+$$
+DELIMITER ;
