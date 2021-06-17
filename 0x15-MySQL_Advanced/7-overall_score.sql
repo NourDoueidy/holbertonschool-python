73dc34fdb41c12466d323576dfc5fdfbee87c6ea
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-17 12:40:32 +0300

    Update 7-overall_score.sql

--- 0x15-MySQL_Advanced/7-overall_score.sql
+++ 0x15-MySQL_Advanced/7-overall_score.sql
@@ -1,13 +1,13 @@
 -- Overall score
 -- Create procedure to compute overall score
-DELIMITER ##
-CREATE PROCEDURE ComputeOverallScoreForUser (
-    IN user_id INT
-)
+DELIMITER $$
+
+CREATE PROCEDURE ComputeOverallScoreForUser (IN user_id INT)
 BEGIN
-    UPDATE users
-    SET overall_score = (SELECT SUM(score)
-        FROM corrections WHERE corrections.user_id = user_id)
-    WHERE id = user_id;
-END ##
+      UPDATE users
+      SET overall_score = (SELECT SUM(score) FROM corrections WHERE corrections.user_id = user_id)
+      WHERE id = user_id;
+    
+END $$
+
 DELIMITER ;
