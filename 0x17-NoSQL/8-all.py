f6af3ddfccf9fb96b38cb659ad34f89535018ee1
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-22 21:18:58 +0300

    Update 8-all.py

--- 0x17-NoSQL/8-all.py
+++ 0x17-NoSQL/8-all.py
@@ -3,6 +3,6 @@
 
 
 def list_all(mongo_collection):
-"""list all documents in a collection"""
+    """list all documents in a collection"""
     documents = mongo_collection.find()
     return documents
