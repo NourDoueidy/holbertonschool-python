884829062ee207ee9e504d4649148892737f5553
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-23 12:30:22 +0300

    Update 0-rotate_2d_matrix.py

--- 0x18-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x18-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,7 +5,7 @@
 def rotate_2d_matrix(matrix):
     """function that rotates matrix 90 degrees in-place"""
     matrix.reverse()
-    for i in range len(matrix):
+    for i in range(len(matrix)):
         for j in range (i):
             matrix [i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
     return matrix
