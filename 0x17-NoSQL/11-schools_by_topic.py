662fa23842d53510fd161d49225047ec63d48ca1
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-23 11:24:26 +0300

    Update 11-schools_by_topic.py

--- 0x17-NoSQL/11-schools_by_topic.py
+++ 0x17-NoSQL/11-schools_by_topic.py
@@ -4,5 +4,5 @@
 
 def schools_by_topic(mongo_collection, topic):
     """function that returns the list of school having a specific topic"""
-    list_all = mongo_collection.find({"topic": topic})
+    list_all = mongo_collection.find({"topics": topic})
     return list_all
