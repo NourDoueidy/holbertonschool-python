5169877653e21c76ba82a62091cbecd2caf2edcd
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-23 10:27:49 +0300

    Update 9-insert_school.py

--- 0x17-NoSQL/9-insert_school.py
+++ 0x17-NoSQL/9-insert_school.py
@@ -2,7 +2,7 @@
 """insert in pymongo"""
 
 
-def insert_school(mongo_collecTion, **kwargs):
+def insert_school(mongo_collection, **kwargs):
     """Function that inserts a new document in a collection based on kwargs"""
     document_id = mongo_collection.insert(kwargs)
     return document_id
