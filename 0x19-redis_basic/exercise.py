8fee05464fa9d23bac47090d151692f4218b61f5
NourDoueidy <nourdoueidy@hotmail.com>
2021-06-25 09:51:29 +0300

    completed task

---
+++ 0x19-redis_basic/exercise.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env pyrhon3
+"""Redis Exercise"""
+
+
+class Cache:
+    """Defines a class Cache"""
+
+    def __init__(self):
+        """Initializer"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """generate random key and store the input data and return key"""
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
