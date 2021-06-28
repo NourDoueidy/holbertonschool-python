0f3757d9b104c85308ae65265bbc41cbb94c9ee7
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-28 08:52:54 +0300

    Update exercise.py

--- 0x19-redis_basic/exercise.py
+++ 0x19-redis_basic/exercise.py
@@ -7,12 +7,14 @@ from typing import Union
 
 class Cache():
     """Defines a class Cache"""
-
+    
     def __init__(self):
         """Initializer"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """generate random key and store the input data and return key"""
         key = str(uuid.uuid4())
