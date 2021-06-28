6e2ff074a8ed85159626cafa9ad1fdf32502760a
NourDoueidy <79709029+NourDoueidy@users.noreply.github.com>
2021-06-28 09:02:09 +0300

    Update exercise.py

--- 0x19-redis_basic/exercise.py
+++ 0x19-redis_basic/exercise.py
@@ -12,7 +12,8 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         ''' def store '''
         gen = str(uuid.uuid4())
