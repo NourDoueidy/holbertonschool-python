416df55a02cbf1323bca506b08308334d3bfaf56
NourDoueidy <nour.doueidy@gmail.com>
2021-06-29 22:29:58 +0300

    completed task

---
+++ 0x1A-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Basic Dicionary"""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Defines the class"""
+
+    def put(self, key item):
+        """method"""
+        if key and item:
+             self.cache_data[key] = item
+
+    def get(self, key):
+        """method"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
