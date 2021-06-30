cfc5e436a7e5e25ba031e0d8788059ca5c09a936
NourDoueidy <nourdoueidy@hotmail.com>
2021-06-30 10:25:38 +0300

    completed task

---
+++ 0x1A-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Fifo Caching"""
+
+BaseCaching = __import__ ("base_caching").BaseCaching
+
+
+class FifoCache(BaseCaching):
+    """Defines a class"""
+
+    def __init__(self):
+        """Initializer"""
+        super().__init__()
+        self.key_indexes = []
+
+    def put(self, key, item):
+        """self descriptive"""
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            item_discarded = self.key_indexes.pop(0)
+            del self.cache_data[itm_discarded]
+            print("Discard: {}".format(item_discarded)
+
+        self.cache_data[key] = item
+        self.key_indexes.append(key)
+ 
+    def get(self, key):
+        """self descriptive"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
