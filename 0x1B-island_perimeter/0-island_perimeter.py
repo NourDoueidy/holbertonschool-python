91fc924a134b185ea71e600da219384c3ef22136
NourDoueidy <nourdoueidy@hotmail.com>
2021-07-02 12:26:14 +0300

    task

---
+++ 0x1B-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' island perimeter '''
+
+def island_perimeter(grid):
+    ''' returns the perimeter of the island decribed in grid '''
+    c = 0
+    for i, n in enumerate(grid):
+        for j, n in enumerate(grid):
+        
